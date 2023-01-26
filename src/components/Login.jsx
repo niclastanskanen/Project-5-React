0fa9753267f76a7de84767f8d4f516db29ffc91e
niclastanskanen <niclastanskanen@hotmail.com>
2023-01-26 09:28:55 +0000

    add login components with video, logo and buttons

---
+++ src/components/Login.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AiOutlineLogin, AiOutlineUserAdd } from 'react-icons/ai';
+import shareVideo from '../assets/share.mp4'
+import logo from '../assets/logo-white.png'
+
+
+const Login = () => {
+  return (
+    <div className='flex justify-start items-center flex-col h-screen'>
+        <div className='relative w-full h-full'>
+          <video
+            src={shareVideo}
+            type='video/mp4'
+            loop
+            controls={false}
+            muted
+            autoPlay
+            className='w-full h-full object-cover'
+          />
+          <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+            <div className='p-5'>
+              <img 
+                src={logo}
+                width='230px'
+                alt='logo'
+              />
+            </div>
+            <div className='shadow-2x1 pb-4'>
+              <button
+                type='button'
+                className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'
+              >
+                <AiOutlineLogin className='mr-4'/> Sign In
+              </button>
+            </div>
+            <div className='shadow-2x1'>
+              <button
+                type='button'
+                className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'
+              >
+                <AiOutlineUserAdd className='mr-4'/> Create an account
+              </button>
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
