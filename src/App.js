ae8059af7c9f82a595fde35f0cc03195352a46dd
niclastanskanen <niclastanskanen@hotmail.com>
2023-01-26 08:58:37 +0000

    add routes for login and all other pages with react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routes>
+      <Route path='login' element={<Login />} />
+      <Route path='/*' element={<Home />} />
+    </Routes>
   )
 }
 
