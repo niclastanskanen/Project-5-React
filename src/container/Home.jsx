e66079a24354279237ad8e5a4103d4c54a53d4f2
niclastanskanen <niclastanskanen@hotmail.com>
2023-01-31 08:42:09 +0000

    add style to sidebar with responsive for mobile view

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -9,7 +9,12 @@ import Logo from '../assets/logo.png';
 const Home = () => {
   return (
     <div className='flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out'>
-      Home
+      <div className='hidden md:flex h-screen flex-initial'>
+        <Sidebar />
+      </div>
+      <div className='flex md:hidden flex-row'>
+        <AiOutlineMenu fontSize={40} className='cursor-pointer' onClick={() => setToggleSidebar(false)} />
+      </div>
     </div>
   )
 }
