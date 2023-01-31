898b71a03d55319213ea749ad4c48fe1d4604c3e
niclastanskanen <niclastanskanen@hotmail.com>
2023-01-31 15:14:21 +0000

    add loading circlesr from react loader spinner with info message

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -5,6 +5,10 @@ import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
 
 const Image = () => {
+  const [loading, setLoading] = useState(true);
+
+  if(loading) return <Spinner message='Loading more photos to your feed!'/>
+
   return (
     <div>Image</div>
   )
--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Circles } from 'react-loader-spinner';
 
-const Spinner = () => {
+const Spinner = ({ message }) => {
   return (
-    <div>Spinner</div>
+    <div className='flex flex-col justify-center items-center w-full h-full'>
+        <Circles
+            type="Circles"
+            color='#E77975'
+            height={50}
+            width={200}
+            className='m-5'
+        />
+        <p className='text-lg text-center pt-5 px-2'>
+            {message}
+        </p>
+    </div>
   )
 }
 
