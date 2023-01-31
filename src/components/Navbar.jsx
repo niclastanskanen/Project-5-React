97427ed6eb862257f2bb398aa0e679bd7718d858
niclastanskanen <niclastanskanen@hotmail.com>
2023-01-31 14:45:31 +0000

    add style to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,8 +23,11 @@ const Navbar = ({ searchTerm, setSearchTerm }) => {
         />
       </div>
       <div className='flex gap-3'>
-        <Link to={`/profiles/${currentUser?.profile_id}`}>
+        <Link to={`/profiles/${currentUser?.profile_id}`} className='hidden md:block'>
         <img src={currentUser?.profile_image} alt='user image' className='w-14 h-12 rounded-lg' />
+        </Link>
+        <Link to='/upload' className='bg-red-400 text-white rounded-lg w-12 h-12 md:w-14 md:h-12 flex justify-center items-center'>
+          <IoMdAddCircleOutline fontSize={30}/>
         </Link>
       </div>
     </div>
