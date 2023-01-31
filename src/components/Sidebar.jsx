9821a28fe41e394b90a6a61a5f9e0f0e7c602b90
niclastanskanen <niclastanskanen@hotmail.com>
2023-01-31 10:15:28 +0000

    add category to sidebar with boilerplates for categories

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,6 +4,19 @@ import { AiOutlineHome, AiOutlineArrowRight } from 'react-icons/ai';
 
 import logo from '../assets/logo.png';
 
+const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
+const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize';
+
+const categories = [
+  { name: 'Art'},
+  { name: 'Cats'},
+  { name: 'Dogs'},
+  { name: 'Food'},
+  { name: 'Nature'},
+  { name: 'Photo'},
+  { name: 'Travel'},
+  { name: 'Wallpaper'},
+]
 
 const Sidebar = ({ closeToggle }) => {
   const handleCloseSidebar = () => {
@@ -24,9 +37,23 @@ const Sidebar = ({ closeToggle }) => {
           <div className='flex flex-col gap-5'>
             <NavLink
               to='/'
+              className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle}
+              onClick={handleCloseSidebar}
             >
-
+              <AiOutlineHome />
+              Home
             </NavLink>
+            <h3 className='mt-2 px-5 text-base 2xl:text-xl'>Categories</h3>
+            {categories.slice(0, categories.length -1).map((category) =>(
+              <NavLink
+              to={`/category/${category.name}`}
+              className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle}
+              onClick={handleCloseSidebar}
+              key={category.name}
+              >
+                {category.name}
+              </NavLink>
+            ))}
           </div>
         </div>
     </div>
