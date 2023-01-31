09ac4f66312aaaf7cd593ce896ca9b90641491fb
niclastanskanen <niclastanskanen@hotmail.com>
2023-01-31 16:19:56 +0000

    fix image components failed with code 401

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,28 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate, Link } from 'react-router-dom';
-import { useCurrentUser } from '../contexts/CurrentUserContext';
-import Avatar from './Avatar';
-
+import React, { useState } from 'react';
 
 import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
 
-const Image = (props) => {
-  const {
-
-    owner,
-    profile_id,
-    profile_image,
-    title,
-    content,
-    image,
-    updated_at,
-
-  } = props;
-
-  const currentUser = useCurrentUser();
-  const is_owner = currentUser?.username === owner;
-  const history = useNavigate();
+const Image = () => {
 
   const [loading, setLoading] = useState(false);
 
@@ -30,12 +11,7 @@ const Image = (props) => {
 
   return (
     <div>
-      <div className="align-items-center justify-content-between">
-          <Link to={`/profiles/${profile_id}`}>
-            <Avatar src={profile_image} height={55} />
-            {owner}
-          </Link>
-        </div>
+        <MasonryLayout />
     </div>
   )
 }
