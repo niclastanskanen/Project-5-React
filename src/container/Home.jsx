0090a5a5bb6fccb68c2ac55f5124be30e5f96c48
niclastanskanen <niclastanskanen@hotmail.com>
2023-01-31 09:43:09 +0000

    add profile and feed routes in home container

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -5,13 +5,18 @@ import { AiOutlineCloseCircle } from 'react-icons/ai';
 import { Sidebar, Profile } from '../components';
 import Feed from './Feed';
 import logo from '../assets/logo.png'
-import { Link } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import { useCurrentUser } from '../contexts/CurrentUserContext';
 import Avatar from '../components/Avatar';
 
 const Home = () => {
   const currentUser = useCurrentUser();
   const [toggleSidebar, setToggleSidebar] = useState(false);
+  const scrollRef = useRef(null);
+
+  useEffect(() => {
+    scrollRef.current.scrollTo(0, 0)
+  }, []);
 
   return (
     <div className='flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out'>
@@ -19,22 +24,30 @@ const Home = () => {
         <Sidebar />
       </div>
       <div className='flex md:hidden flex-row'>
-        <AiOutlineMenu fontSize={40} className='cursor-pointer' onClick={() => setToggleSidebar(true)} />
-        <Link to='/'>
-          <img src={logo} alt='logo' className='w-40' />
-        </Link>
-        <Link to={`/profiles/${currentUser?.profile_id}`}>
-        <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
-        </Link>
-      </div>
-      {toggleSidebar && (
-        <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in'>
-          <div className='absolute w-full flex justify-end items-center p-2'>
-            <AiOutlineCloseCircle fontSize={30} className='cursor-pointer' onClick={() => setToggleSidebar(false)} />
-          </div>
-          <Sidebar closeToggle={setToggleSidebar} />
+        <div className='p-2 w-full flex flex-row justify-between items-center shadow-md'>
+          <AiOutlineMenu fontSize={40} className='cursor-pointer' onClick={() => setToggleSidebar(true)} />
+          <Link to='/'>
+            <img src={logo} alt='logo' className='w-40' />
+          </Link>
+          <Link to={`/profiles/${currentUser?.profile_id}`}>
+            <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+          </Link>
         </div>
-      )}
+        {toggleSidebar && (
+          <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in'>
+            <div className='absolute w-full flex justify-end items-center p-2'>
+              <AiOutlineCloseCircle fontSize={30} className='cursor-pointer' onClick={() => setToggleSidebar(false)} />
+            </div>
+            <Sidebar closeToggle={setToggleSidebar} />
+          </div>
+        )}
+      </div>
+      <div className='pb-2 flex-1 h-screen overflow-y-scroll' ref={scrollRef}>
+        <Routes>
+          <Route path='/profiles/:id' element={<Profile />} />
+          <Route path='/*' element={<Feed />} />
+        </Routes>
+      </div>
     </div>
   )
 }
