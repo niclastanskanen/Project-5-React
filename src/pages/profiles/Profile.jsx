11103a9e23ae874a7684517aa296b04dff9e15f5
niclastanskanen <niclastanskanen@hotmail.com>
2023-02-01 13:19:34 +0000

    add basic function to profile page to show user image, id posts

--- src/pages/profiles/Profile.jsx
+++ src/pages/profiles/Profile.jsx
@@ -1,9 +1,92 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useProfileData, useSetProfileData } from '../../contexts/ProfileDataContext';
+import { axiosReq } from '../../api/axiosDefaults';
+import { useParams } from 'react-router-dom';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import Spinner from '../../components/Spinner';
+import { fetchMoreData } from '../../utils/utils';
+import { Navbar } from '../../components';
 
-const Profile = () => {
+
+function Profile() {
+
+  const [profilePosts, setProfilePosts] = useState({ results: [] });
+
+  const currentUser = useCurrentUser();
+  const { id } = useParams();
+
+  const { setProfileData } = useSetProfileData();
+  const { pageProfile } = useProfileData();
+
+  const [profile] = pageProfile.results;
+  const is_owner = currentUser?.username === profile?.owner;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [{ data: pageProfile }, { data: profilePosts }] =
+          await Promise.all([
+            axiosReq.get(`/profiles/${id}/`),
+            axiosReq.get(`/posts/?owner__profile=${id}`),
+          ]);
+        setProfileData((prevState) => ({
+          ...prevState,
+          pageProfile: { results: [pageProfile] },
+        }));
+        setProfilePosts(profilePosts);
+      } catch (err) {
+        // console.log(err);
+      }
+    };
+    fetchData();
+  }, [id, setProfileData]);
+
+  const mainProfile = (
+    <>
+      {profile?.is_owner}
+      <div>
+      <img  src={profile?.image} />
+      <h3>{profile?.owner}</h3>
+      <p>{profile?.posts_count}</p>
+      <p>{profile?.followers_count}</p>
+      <p>{profile?.following_count}</p>
+      <div lg={3} className="text-lg-right">
+          {currentUser &&
+            !is_owner &&
+            (profile?.following_id ? (
+              <button
+                className=''
+
+              >
+                unfollow
+              </button>
+            ) : (
+              <button
+                className=''
+
+              >
+                follow
+              </button>
+            ))}
+        </div>
+      </div>
+    </>
+  );
+
+  const mainProfilePosts = (
+    <>
+      <p>{profile?.owner}</p>
+      <hr />
+    </>
+  )
+
   return (
+
     <div>
-        Profile
+          <Navbar />
+        {profile?.content && <col className="p-3">{profile.content}</col>}
+        {mainProfile}
+        {mainProfilePosts}
     </div>
   )
 }
