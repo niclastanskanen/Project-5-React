a843544c75791394cff991852703ff81b759ac53
niclastanskanen <niclastanskanen@hotmail.com>
2023-02-02 16:22:55 +0000

    fix api reqeust axios

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
 
-const Image = () => {
+import { useCurrentUser } from "../contexts/CurrentUserContext";
+import axios from "axios";
+
+import { Link } from "react-router-dom";
+
+
+const Image = (props) => {
+  const {
+    id,
+    owner,
+    profile_id,
+    profile_image,
+    comments_count,
+    likes_count,
+    like_id,
+    title,
+    content,
+    image,
+    updated_at,
+    postPage,
+    setPosts,
+  } = props;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
+
+
   return (
-    <div>Image</div>
-  )
-}
+    <div className=''>
+      <div>
+        <div className="align-items-center justify-content-between">
+          <Link to={`/profiles/${profile_id}`}>
+            <img src={profile_image} height={55} />
+            {owner}
+          </Link>
+          <div className="d-flex align-items-center">
+            <span>{updated_at}</span>
+            {is_owner && postPage && "..."}
+          </div>
+        </div>
+      </div>
+      <Link to={`/posts/${id}`}>
+        <img src={image} alt={title} />
+      </Link>
 
-export default Image
+    </div>
+  );
+};
+
+export default Image;
--- src/components/ImageDetail.jsx
+++ src/components/ImageDetail.jsx
@@ -5,13 +5,33 @@ import { v4 as uuidv4 } from 'uuid';
 import MasonryLayout from './MasonryLayout';
 import { AiOutlineDownload } from 'react-icons/ai';
 import Spinner from './Spinner';
+import { axiosReq } from '../api/axiosDefaults';
+import Image from './Image';
 
-const ImageDetail = () => {
+function ImageDetail () {
+  const { id } = useParams();
+  const [post, setPost] = useState({ results: [] });
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: post }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+        ]);
+        setPost({ results: [post] });
+        console.log(post);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [id]);
   
   
   return (
     <div>
-      hello
+      <img {...post.results[0]} setPosts={setPost} postPage />
     </div>
   )
 }
