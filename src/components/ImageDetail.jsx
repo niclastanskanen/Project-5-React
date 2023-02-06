127c1d9273f3a03313768ff3f4e2f0736cbb2295
niclastanskanen <niclastanskanen@hotmail.com>
2023-02-06 21:32:21 +0000

    fix image detail with axios request

--- src/components/ImageDetail.jsx
+++ src/components/ImageDetail.jsx
@@ -1,93 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-
+import React, { useEffect, useState } from 'react';
+import { useCurrentUser } from '../contexts/CurrentUserContext';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { fetchMoreData } from '../utils/utils';
+import { useParams } from 'react-router-dom';
+import ProfilePost from '../pages/profiles/ProfilePost';
 import { axiosReq } from '../api/axiosDefaults';
 
 
-const ImageDetail = ({ user }) => {
-  const [errors, setErrors] = useState({});
 
-  const [postData, setPostData] = useState({
-    title: "",
-    content: "",
-    image: "",
-  });
-  const { title, content, image } = postData;
-
-  const imageInput = useRef(null);
-  const navigate = useNavigate
+function ImageDetail() {
   const { id } = useParams();
+  const [post, setPost] = useState({ results: [] });
+
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const { data } = await axiosReq.get(`/posts/${id}`);
-        const { title, content, image, is_owner } = data;
-
-        is_owner ? setPostData({ title, content, image }) : navigate("/");
+        const [{ data: post }, { data: comments }] = await Promise.all([
+          axiosReq.get(`/posts/${id}/`),
+          axiosReq.get(`/comments/?post=${id}`),
+        ]);
+        setPost({ results: [post] });
+        setComments(comments);
       } catch (err) {
-
+        console.log(err);
       }
     };
 
     handleMount();
-  }, [navigate, id]);
+  }, [id]);
 
-  const handleChange = (event) => {
-    setPostData({
-      ...postData,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  const handleChangeImage = (event) => {
-    if (event.target.files.length) {
-      URL.revokeObjectURL(image);
-      setPostData({
-        ...postData,
-        image: URL.createObjectURL(event.target.files[0]),
-      });
-    }
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const formData = new FormData();
-
-    formData.append("title", title);
-    formData.append("content", content);
-
-    if (imageInput?.current?.files[0]) {
-      formData.append("image", imageInput.current.files[0]);
-    }
-
-    try {
-      await axiosReq.put(`/posts/${id}`, formData);
-      navigate(`/posts/${id}`);
-    } catch (err) {
-
-      if (err.response?.status !== 401) {
-        setErrors(err.response?.data);
-      }
-    }
-  };
-
-
-  
   return (
     <div>
-      <h1>ImageDetail</h1>
-      <form
-        type='text'
-        name='title'
-        value={title}
-        onChange={handleChange}
-      >
-
-      </form>
+      <ProfilePost {...post.results[0]} setPosts={setPost} postPage />
     </div>
-
-  );
-};
+  )
+}
 
 export default ImageDetail
