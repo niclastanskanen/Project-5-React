8e1900603c9b1cb7b6a26462a236254c84a1e204
niclastanskanen <niclastanskanen@hotmail.com>
2023-02-06 11:40:42 +0000

    add profileposts style and image full width

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -16,7 +16,7 @@ export const Dropdown = ({ handleEdit, handleDelete }) => {
     return (
 
         <div
-            className="flex gap-10 p-2 items-center bg-white rounded-lg shadow-lg mx-3 justify-center items-center"
+            className='flex gap-10 p-2 items-center rounded-lg mx-3 justify-center items-center'
             as={Options}
         >
             <AiOutlineEdit className='cursor-pointer' fontSize={34} onClick={handleEdit}>Edit</AiOutlineEdit>
--- src/pages/profiles/ProfilePost.jsx
+++ src/pages/profiles/ProfilePost.jsx
@@ -7,66 +7,70 @@ import Dropdown from '../../components/Dropdown';
 
 
 const ProfilePost = (props) => {
-    const {
-        id,
-        owner,
-        profile_id,
-        profile_image,
-        comments_count,
-        likes_count,
-        like_id,
-        title,
-        content,
-        image,
-        updated_at,
-        postPage,
-        setPosts,
-      } = props;
+  const {
+    id,
+    owner,
+    profile_id,
+    profile_image,
+    comments_count,
+    likes_count,
+    like_id,
+    title,
+    content,
+    image,
+    updated_at,
+    postPage,
+    setPosts,
+  } = props;
 
-      const currentUser = useCurrentUser();
-      const is_owner = currentUser?.username === owner;
-      const navigate = useNavigate();
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+  const navigate = useNavigate();
 
-      const handleEdit = () => {
-        navigate(`/posts/${id}/edit`);
-      };
-      
-      const handleDelete = async () => {
-        try {
-          await axiosRes.delete(`/posts/${id}`);
-          navigate(-1);
-        } catch (err) {
-          // console.log(err);
-        }
-      };
+  const handleEdit = () => {
+    navigate(`/posts/${id}/edit`);
+  };
 
-      const breakpointColumnsObj = {
-        default: 4,
-        3000: 6,
-        2000: 5,
-        1200: 3,
-        1000: 2,
-        500: 1,
-      };
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/posts/${id}`);
+      navigate(-1);
+    } catch (err) {
+      // console.log(err);
+    }
+  };
+
+  const breakpointColumnsObj = {
+    default: 4,
+    3000: 6,
+    2000: 5,
+    1200: 3,
+    1000: 2,
+    500: 1,
+  };
 
   return (
-    <div className='px-2'>
-      <Masonry className="flex animate-slide-fwd" breakpointCols={breakpointColumnsObj}>
-        
-      <p>{updated_at}</p>
-      <p>{title}</p> 
-      <p>{content}</p>
-    
-    <img src={image} alt={title} className='w-max' />
 
-    <p>{likes_count}</p>
-    <p>{comments_count}</p>
-    <Dropdown
-      handleEdit={handleEdit}
-      handleDelete={handleDelete}
-    />
-    </Masonry>
-    </div>
+
+      <div class="max-w rounded overflow-hidden shadow-lg">
+        <img class="w-max" src={image} alt={title} />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">{title}</div>
+          <p class="text-gray-700 text-base">
+            {content}
+          </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{likes_count} Likes</span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{comments_count} Comments</span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{updated_at}</span>
+        </div>
+        <div>
+          <Dropdown handleEdit={handleEdit} handleDelete={handleDelete} />
+        </div>
+      </div>
+
+
 
   )
 }
