b249e1d96fc4120d8ff331634ca46c065189bcbb
niclastanskanen <niclastanskanen@hotmail.com>
2023-02-06 15:49:12 +0000

    add image feed at homepage

---
+++ src/components/ImageFeed.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Search from "./Search";
+
+
+
+const ImageFeed = () => {
+
+  return (
+    <div>
+    <h1>Image Feed</h1>
+    <Search />
+
+    </div>
+  );
+};
+
+export default ImageFeed;
