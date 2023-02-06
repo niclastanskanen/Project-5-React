ef4c7367b1db594c7d2502be5d8897cc26a40d50
niclastanskanen <niclastanskanen@hotmail.com>
2023-02-06 09:02:42 +0000

    fix currentuser navigate at homepage for loggedout users

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import './api/axiosDefaults';
-
-import Login from './components/Login';
-import Home from './container/Home';
 import { useCurrentUser } from './contexts/CurrentUserContext';
+
+import Home from './container/Home';
+
 import SignInForm from './pages/auth/SignInForm';
 import SignUpForm from './pages/auth/SignUpForm';
+import { Login } from './components';
 
 const App = () => {
-  const navigate = useNavigate();
-  const currentUser = useCurrentUser();
+  // const navigate = useNavigate();
+  // const currentUser = useCurrentUser();
 
-
+  // useEffect(() => {
+  //   if (!currentUser) navigate('/login');
+  // }, [currentUser, navigate]);
 
   return (
     <Routes>
-      <Route path='login' element={<Login />} />
-      <Route path='/*' element={<Home />} />
+      <Route path='/login' element={<Login />} />
       <Route path='/signin' element={<SignInForm />} />
       <Route path='/signup' element={<SignUpForm />} />
+      <Route path='/*' element={<Home />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
