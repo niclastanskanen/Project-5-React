96b4f914b8c3bb4f95b2544bbb17f01d8b83752f
niclastanskanen <niclastanskanen@hotmail.com>
2023-03-14 18:16:21 +0000

    fix startpage to show login page when user is not logged in

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './api/axiosDefaults';
-// import { useCurrentUser } from './contexts/CurrentUserContext';
 
 import Home from './container/Home';
-
+import { useCurrentUser } from './contexts/CurrentUserContext';
 import SignInForm from './pages/auth/SignInForm';
 import SignUpForm from './pages/auth/SignUpForm';
 import { Login } from './components';
 
-const App = () => {
-  // const navigate = useNavigate();
-  // const currentUser = useCurrentUser();
 
-  // useEffect(() => {
-  //   if (!currentUser) navigate('/login');
-  // }, [currentUser, navigate]);
+const App = () => {
+  const currentUser = useCurrentUser();
 
   return (
     <Routes>
+      <Route
+        path='/*'
+        element={currentUser
+          ? <Home />
+          : <Navigate to='/login' replace />
+        }
+      />
       <Route path='/login' element={<Login />} />
       <Route path='/signin' element={<SignInForm />} />
       <Route path='/signup' element={<SignUpForm />} />
-      <Route path='/*' element={<Home />} />
     </Routes>
   );
 };
